bf3437d5ec6b9e7a80e5cae9d36c8e4e469edfe9
Anurudddha468 <anuruddhagayan468@gmail.com>
2024-04-01 19:25:13 +0530

    Anotation Practical (2024.03.30)

--- CoursePracticals/src/test/java/Day_02/TestNGAnotation.java
+++ CoursePracticals/src/test/java/Day_02/TestNGAnotation.java
@@ -1,26 +1,45 @@
-package CoursePackage;
+package Day_02;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
-public class CoursePractical {
+public class TestNGAnotation {
 
-    @Test
-    public void LoginPage(){
+    private WebDriver driver;
 
+    @BeforeSuite
+    public void beforeSuite(){
         //Setup web browser driver
         WebDriverManager.chromedriver().setup();
-
+    }
+    @BeforeMethod
+    public void beforMethod(){
         //Open a web browser (Chrome Broser)
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
 
         //Navigate to the web page
         driver.get("https://www.saucedemo.com/");
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        if(driver!=null){
+            driver.quit();
+        }
+        //driver.close();
+    }
+
+    @Test
+    public void LoginPage(){
 
         //Type username
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
@@ -36,14 +55,11 @@ public class CoursePractical {
 
         //Verify landing page
         Assert.assertEquals(driver.findElement(By.cssSelector("span.title")).getText(),"Products");
-       // Assert.assertEquals(driver.findElement(By.xpath("//span[@class=\"title\"]")).getText(),"Products");
-
-        driver.quit();
 
     }
 
-    @AfterMethod
-    public void aftermethod(){
-
-    }
+    /*@Test
+    public void testmethod2(){
+        System.out.println("Test method 2");
+    }*/
 }
